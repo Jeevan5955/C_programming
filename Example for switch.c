9bf90c5178257bf241ce98d42a221a7fde154736
Jeevan5955 <54932235+Jeevan5955@users.noreply.github.com>
2020-07-03 19:43:54 +0530

    Create Example for switch.c

---
+++ Example for switch.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int weekday;
+while(1)
+{
+printf("Enter weekday: ");
+scanf("%d",&weekday);
+switch(weekday)
+{
+case 1: printf("Monday");
+break;
+case 2: printf("Tuesday");
+break;
+case 3: printf("Wednesday");
+break;
+case 4: printf("Thursday");
+break;
+case 5: printf("Friday");
+break;
+case 6: printf("Saturday");
+break;
+case 7: printf("Sunday");
+break;
+default: printf("invslid entry");
+}
+}
+}
